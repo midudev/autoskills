c2d2f20f0d43b19522a8538761d3b471c22c7af5
Jannael Orlando <orlandojannael@gmail.com>
2026-04-23 11:04:07 -0600

    feat: get cli version func

---
+++ src/utils/get-cli-version.ts
@@ -0,0 +1,9 @@
+export async function getCliVersion() {
+  const { default: cliPkg } = await import(
+    "../../packages/autoskills/package.json",
+    { with: { type: "json" } }
+  );
+  const cliVersion = cliPkg.version;
+
+
+}
